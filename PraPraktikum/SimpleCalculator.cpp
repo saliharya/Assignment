addc37d928eee38f9c441a471db60b75c49ae6ec
Salih Arya Gumilang <saliharyazinc@gmail.com>
2022-03-18 23:06:12 +0700

    feature added: terminate the program by selecting 0

--- PraPraktikum/SimpleCalculator.cpp
+++ PraPraktikum/SimpleCalculator.cpp
@@ -12,7 +12,8 @@ void Jumlah()
     cout << " Masukkan Bilangan Kedua : ";
     cin >> y;
     hasil = x + y;
-    cout << " Hasil Penghitungan : " << hasil;
+    cout << " Hasil Penghitungan : " << hasil << endl
+         << endl;
 }
 void Kurang()
 {
@@ -24,7 +25,8 @@ void Kurang()
     cout << " Masukkan Bilangan Kedua : ";
     cin >> y;
     hasil = x - y;
-    cout << " Hasil Penghitungan : " << hasil;
+    cout << " Hasil Penghitungan : " << hasil << endl
+         << endl;
 }
 void Kali()
 {
@@ -36,84 +38,98 @@ void Kali()
     cout << " Masukkan Bilangan Kedua : ";
     cin >> y;
     hasil = x * y;
-    cout << " Hasil Penghitungan : " << hasil;
+    cout << " Hasil Penghitungan : " << hasil << endl
+         << endl;
 }
-void Bagi() 
+void Bagi()
 {
-	float x, y;
-	float hasil;
-	cout << " Pembagian\n\n";
-	cout << " Masukkan Bilangan Pertama : ";
-	cin >> x;
-	cout << " Masukkan Bilangan Kedua : ";
-	cin >> y;
-	hasil = x / y;
-	cout << " Hasil Penghitungan : " << hasil;
+    float x, y;
+    float hasil;
+    cout << " Pembagian\n\n";
+    cout << " Masukkan Bilangan Pertama : ";
+    cin >> x;
+    cout << " Masukkan Bilangan Kedua : ";
+    cin >> y;
+    hasil = x / y;
+    cout << " Hasil Penghitungan : " << hasil << endl
+         << endl;
 }
-void Pangkat() 
+void Pangkat()
 {
-	int bil;
-	int pngkt;
-	float hasil = 0;
-	cout << " Perpangkatan\n\n";
-	cout << " Masukkan Bilangan : ";
-	cin >> bil;
-	cout << " Masukkan Pangkat : ";
-	cin >> pngkt;
-	hasil = pow(bil, pngkt);
-	cout << " Hasil Penghitungan : " << hasil;
+    int bil;
+    int pngkt;
+    float hasil = 0;
+    cout << " Perpangkatan\n\n";
+    cout << " Masukkan Bilangan : ";
+    cin >> bil;
+    cout << " Masukkan Pangkat : ";
+    cin >> pngkt;
+    hasil = pow(bil, pngkt);
+    cout << " Hasil Penghitungan : " << hasil << endl
+         << endl;
 }
-void Akar() 
+void Akar()
 {
-	float bil;
-	float hasil = 0;
-	cout << " Akar Pangkat\n\n";
-	cout << " Masukkan Bilangan : ";
-	cin >> bil;
-	hasil = sqrt(bil);
-	cout << " Hasil Penghitungan : " << hasil;
+    float bil;
+    float hasil = 0;
+    cout << " Akar Pangkat\n\n";
+    cout << " Masukkan Bilangan : ";
+    cin >> bil;
+    hasil = sqrt(bil);
+    cout << " Hasil Penghitungan : " << hasil << endl
+         << endl;
 }
 
-int main() {
-	int pil;
-	char pil2;
+int main()
+{
+    int pil;
+    char pil2;
 
-	cout << " ===== KALKULATOR SEDERHANA =====\n";
-	cout << " Pilih Operasi Aritmatika\n" <<
-    endl;
-	cout << " 1. Penjumlahan\n";
-	cout << " 2. Pengurangan\n";
-	cout << " 3. Perkalian\n";
-	cout << " 4. Pembagian\n";
-	cout << " 5. Perpangkatan\n";
-	cout << " 6. Akar Pangkat\n";
-	cout << "\n Pilihan Anda : ";
-	cin >> pil;
-	cout << " ====================================" << endl;
-	
-switch (pil) {
-	case 1:
-    Jumlah();
-    break;
-	case 2:
-    Kurang();
-    break;
-	case 3:
-    Kali();
-    break;
-	case 4:
-    Bagi();
-    break;
-	case 5:
-    Pangkat();
-    break;
-	case 6:
-    Akar();
-    break;
-	default:
-    cout << " Pilihan Anda Tidak Ada\n";
-    cout << " Silahkan Jalankan Program dan Input Ulang\n";
-    break;
-  }
-return 0;
+    do
+    {
+        cout << " ===== KALKULATOR SEDERHANA =====\n";
+        cout << " Pilih Operasi Aritmatika\n"
+             << endl;
+        cout << " 1. Penjumlahan\n";
+        cout << " 2. Pengurangan\n";
+        cout << " 3. Perkalian\n";
+        cout << " 4. Pembagian\n";
+        cout << " 5. Perpangkatan\n";
+        cout << " 6. Akar Pangkat\n";
+        cout << " 0. Keluar Program";
+        cout << "\n Pilihan Anda : ";
+        cin >> pil;
+        cout << " ====================================" << endl;
+
+        switch (pil)
+        {
+        case 1:
+            Jumlah();
+            break;
+        case 2:
+            Kurang();
+            break;
+        case 3:
+            Kali();
+            break;
+        case 4:
+            Bagi();
+            break;
+        case 5:
+            Pangkat();
+            break;
+        case 6:
+            Akar();
+            break;
+        case 0:
+            cout << "Keluar Program" << endl;
+            break;
+        default:
+            cout << " Pilihan Anda Tidak Ada\n";
+            cout << " Silahkan Input Ulang\n";
+            break;
+        }
+    } while (pil != 0);
+
+    return 0;
 }
