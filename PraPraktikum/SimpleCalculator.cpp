fab029f504067aa85ada0c156c3c4a8195d55810
Alnanda Kusuma <101874558+alnandakusuma@users.noreply.github.com>
2022-03-18 20:08:01 +0700

    Update SimpleCalculator.cpp

--- PraPraktikum/SimpleCalculator.cpp
+++ PraPraktikum/SimpleCalculator.cpp
@@ -38,3 +38,46 @@ void Kali()
     hasil = x * y;
     cout << " Hasil Penghitungan : " << hasil;
 }
+int main() {
+
+  int pil;
+  char pil2;
+
+  cout << " ===== KALKULATOR SEDERHANA =====\n";
+  cout << " Pilih Operasi Aritmatika\n" <<
+    endl;
+  cout << " 1. Penjumlahan\n";
+  cout << " 2. Pengurangan\n";
+  cout << " 3. Perkalian\n";
+  cout << " 4. Pembagian\n";
+  cout << " 5. Perpangkatan\n";
+  cout << " 6. Akar Pangkat\n";
+  cout << "\n Pilihan Anda : ";
+  cin >> pil;
+  cout << " ====================================" << endl;
+  switch (pil) {
+  case 1:
+    Jumlah();
+    break;
+  case 2:
+    Kurang();
+    break;
+  case 3:
+    Kali();
+    break;
+  case 4:
+    Bagi();
+    break;
+  case 5:
+    Pangkat();
+    break;
+  case 6:
+    Akar();
+    break;
+  default:
+    cout << " Pilihan Anda Tidak Ada\n";
+    cout << " Silahkan Jalankan Program dan Input Ulang\n";
+    break;
+  }
+  return 0;
+}
